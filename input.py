caa3a17421c30906eda4b1af19b516dc28c97f75
ErikRiswantoS <erikriswantosaputra@gmail.com>
2022-11-09 09:06:58 +0700

    input output string

---
+++ input.py
@@ -0,0 +1,12 @@
+#Belajar_Buat_Biodata_Sederhana_Bersama_Saya_ERIKRISWANTOSAPUTRA
+
+print('='*20)
+print('Biodata Sederhana mengunakan python')
+print('='*20)
+#variabel mengunkan input
+Nama = input('Masukan Nama:  ')
+Hobi = input('Masukan Hobi:  ')
+umur = input('Masukan umur:  ')
+#output mengunakan format
+print(f'Halo, Saya {Nama}, Hobi Saya {Hobi} dan umur saya {umur} tahun ')
+
---
+++ string.py
@@ -0,0 +1,7 @@
+# String dan Output bersama sama saya erik riswanto saputra
+Kalimat = "UNIVERSITAS NUSA PUTRA SUKABUMI"
+print(Kalimat[17:22].capitalize(),Kalimat[12:16].capitalize())
+print(Kalimat[1:11],Kalimat[12]+Kalimat[14:16],Kalimat[17]+Kalimat[19:22],Kalimat[23:31])
+print(Kalimat[23:31],Kalimat[17:22],Kalimat[12:16],Kalimat[0:11])
+print(Kalimat[0]+Kalimat[12]+Kalimat[17]+Kalimat[23])
+print(Kalimat[8:11],Kalimat[14:16]+Kalimat[17:19],Kalimat[27:31])
